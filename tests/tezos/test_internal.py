91cbfebab87c88ca3fc9c3a812e89071ecd62264
Thomas Binetruy <tbinetruy@gmail.com>
2022-09-13 22:39:04 +0200

    imported pymich contract loader
    The pymich test folder is not shipeed with pymich on pypi. Thus the
    import failed in CI. I've manually added it here until we find a better
    solution. I also seems that the `ContractLoader.deploy` method was not
    pushed to the pymich repo either. I've re-implemented one. Somehow I
    couldn't get it under 2 parameters rather than 1 in the original test. I
    also didn't what object that method was initially returning, so I
    returned a straight pytezos ContractInterface object and adapted to code
    test to take this into account

--- tests/tezos/test_internal.py
+++ tests/tezos/test_internal.py
@@ -1,30 +1,97 @@
 import pytest
 
-from pymich.test import ContractLoader
-
 from djwebdapp_tezos.models import TezosTransaction
+
+import functools
+from pathlib import Path
+import os
+
+from pymich.compiler import Compiler
+from pytezos import ContractInterface
+from pytezos.contract.result import OperationResult
+
+
+class ContractLoader:
+    """
+    This object provides a Python API over example contracts, for testing.
+    """
+
+    def __init__(self, contract_path):
+        self.contract_path = contract_path
+
+    @functools.cached_property
+    def source(self):
+        with open(self.contract_path) as f:
+            return f.read()
+
+    @functools.cached_property
+    def compiler(self):
+        return Compiler(self.source)
+
+    @functools.cached_property
+    def micheline(self):
+        return self.compiler.compile_contract()
+
+    @functools.cached_property
+    def interface(self):
+        return ContractInterface.from_micheline(self.micheline)
+
+    @property
+    def dummy(self):
+        return self.interface.storage.dummy()
+
+    @property
+    def storage(self):
+        """ Lazy mutable storage, dummy by default. """
+        if '_storage' not in self.__dict__:
+            self._storage = self.dummy
+        return self._storage
+
+    @storage.setter
+    def storage(self, value):
+        self._storage = value
+
+    @classmethod
+    def factory(cls, path):
+        pymich = Path(os.path.dirname(__file__)) / '..'
+        paths = [
+            Path(path),
+            pymich / 'tests' / path,
+            pymich / 'tests' / 'end_to_end' / path,
+        ]
+        for path in paths:
+            if path.exists():
+                return cls(path.absolute())
+
+    def deploy(self, client, using):
+        contract_ci= self.interface.using(**using)
+        opg = contract_ci.originate(initial_storage=self.storage).send(min_confirmations=1)
+        callee_addr = OperationResult.from_operation_group(opg.opg_result)[
+            0
+        ].originated_contracts[0]
+        return client.contract(callee_addr)
 
 
 @pytest.mark.django_db
 def test_internal(client, blockchain, using):
-    loader = ContractLoader.factory('proxy/A.py')
+    loader = ContractLoader('src/djwebdapp_example/tezos/A.py')
     loader.storage['B'] = client.key.public_key_hash()
     loader.storage['C'] = loader.storage['B']
-    A = loader.deploy(client)
+    A = loader.deploy(client, using)
     caller = TezosTransaction.objects.create(
         blockchain=blockchain,
-        address=A.interface.address,
+        address=A.address,
     )
-    B = loader.deploy(client)
-    C = loader.deploy(client)
-    D = loader.deploy(client)
-    opg1 = A.interface.set_B(B.address).send(min_confirmations=1)
-    opg2 = A.interface.set_C(C.address).send(min_confirmations=1)
+    B = loader.deploy(client, using)
+    C = loader.deploy(client, using)
+    D = loader.deploy(client, using)
+    opg1 = A.set_B(B.address).send(min_confirmations=1)
+    opg2 = A.set_C(C.address).send(min_confirmations=1)
     # the following 2 should not be indexed
-    opg3 = B.interface.set_B(D.address).send(min_confirmations=1)
-    opg4 = C.interface.set_C(A.address).send(min_confirmations=1)
+    opg3 = B.set_B(D.address).send(min_confirmations=1)
+    opg4 = C.set_C(A.address).send(min_confirmations=1)
     caller.blockchain.provider.index()
-    opg5 = A.interface.call_B_and_C(dict(
+    opg5 = A.call_B_and_C(dict(
         value_b='B',
         value_c='C',
     )).send(min_confirmations=1)
