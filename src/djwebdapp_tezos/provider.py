1dd8ad9d7253b1e7f9602beb1911d532f0111435
jpic <jpic@yourlabs.org>
2022-10-14 17:50:50 +0200

    Fix: Transaction state confirm to done

--- src/djwebdapp_tezos/provider.py
+++ src/djwebdapp_tezos/provider.py
@@ -72,7 +72,10 @@ class TezosProvider(Provider):
             self.index_contract(level, op, content, number=number)
         elif (
             content['kind'] == 'transaction'
-            and content.get('destination', None) in self.addresses
+            and (
+                content.get('destination', None) in self.addresses
+                or hash in self.hashes
+            )
         ):
             self.index_call(level, op, content, number=number)
 
@@ -217,7 +220,7 @@ class TezosProvider(Provider):
                 call.args = args[call.function]
 
         # save and return call
-        call.save()
+        call.state_set('done')
 
         return call
 
@@ -308,6 +311,7 @@ class TezosProvider(Provider):
         transaction.level = self.head + 1  # it'll be in the next block
         transaction.gas = origination['contents'][0]['fee']
         transaction.hash = origination['hash']
+        transaction.counter = origination['contents'][0]['counter']
         transaction.save()
 
     def send(self, transaction):
