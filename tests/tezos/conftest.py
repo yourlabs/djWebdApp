6a1c649e1c7680714e8a8af4f2de985cdab7eb22
jpic <jpic@yourlabs.org>
2023-01-20 12:12:43 +0100

    Minor multisig test refactors
    - create all bootstrap accounts fixtures
    - move tezos specific test code into tests/tezos
    - s/wait_transaction/deploy_and_index/g

--- tests/tezos/conftest.py
+++ tests/tezos/conftest.py
@@ -1,7 +1,30 @@
+import binascii
 import pytest
 import pytezos
 
 from djwebdapp.models import Blockchain
+
+# This comes from the bake.sh script in the tezos container
+BOOTSTRAP1_IDENTITY="tz1KqTpEZ7Yob7QbPE4Hy4Wo8fHG8LhKxZSx"
+BOOTSTRAP1_PUBLIC="edpkuBknW28nW72KG6RoHtYW7p12T6GKc7nAbwYX5m8Wd9sDVC9yav"
+BOOTSTRAP1_SECRET="edsk3gUfUPyBSfrS9CCgmCiQsTCHGkviBDusMxDJstFtojtc1zcpsh"
+
+BOOTSTRAP2_IDENTITY="tz1gjaF81ZRRvdzjobyfVNsAeSC6PScjfQwN"
+BOOTSTRAP2_PUBLIC="edpktzNbDAUjUk697W7gYg2CRuBQjyPxbEg8dLccYYwKSKvkPvjtV9"
+BOOTSTRAP2_SECRET="edsk39qAm1fiMjgmPkw1EgQYkMzkJezLNewd7PLNHTkr6w9XA2zdfo"
+
+BOOTSTRAP3_IDENTITY="tz1faswCTDciRzE4oJ9jn2Vm2dvjeyA9fUzU"
+BOOTSTRAP3_PUBLIC="edpkuTXkJDGcFd5nh6VvMz8phXxU3Bi7h6hqgywNFi1vZTfQNnS1RV"
+BOOTSTRAP3_SECRET="edsk4ArLQgBTLWG5FJmnGnT689VKoqhXwmDPBuGx3z4cvwU9MmrPZZ"
+
+BOOTSTRAP4_IDENTITY="tz1b7tUupMgCNw2cCLpKTkSD1NZzB5TkP2sv"
+BOOTSTRAP4_PUBLIC="edpkuFrRoDSEbJYgxRtLx2ps82UdaYc1WwfS9sE11yhauZt5DgCHbU"
+BOOTSTRAP4_SECRET="edsk2uqQB9AY4FvioK2YMdfmyMrer5R8mGFyuaLLFfSRo8EoyNdht3"
+
+BOOTSTRAP5_IDENTITY="tz1ddb9NMYHZi5UzPdzTZMYQQZoMub195zgv"
+BOOTSTRAP5_PUBLIC="edpkv8EUUH68jmo3f7Um5PezmfGrRF24gnfLpH3sVNwJnV5bVCxL2n"
+BOOTSTRAP5_SECRET="edsk4QLrcijEffxV31gGdN2HU7UpyJjA8drFoNcmnB28n89YjPNRFm"
+
 
 
 @pytest.fixture
@@ -36,3 +59,55 @@ def blockchain(head):
     )
     blockchain.node_set.get_or_create(endpoint='http://tzlocal:8732')
     return blockchain
+
+
+def create_account(blockchain, name, address, secret_key):
+    def _secret_key(key):
+        exponent = pytezos.Key.from_encoded_key(key).secret_exponent
+        return binascii.b2a_base64(exponent).decode()
+
+    return blockchain.account_set.create(
+        address=address,
+        secret_key=_secret_key(secret_key),
+        name=name,
+    )
+
+
+@pytest.fixture
+def account1(blockchain):
+    return create_account(blockchain, 'account1', BOOTSTRAP1_IDENTITY, BOOTSTRAP1_SECRET)
+
+
+@pytest.fixture
+def account2(blockchain):
+    return create_account(blockchain, 'account2', BOOTSTRAP2_IDENTITY, BOOTSTRAP2_SECRET)
+
+
+@pytest.fixture
+def account3(blockchain):
+    return create_account(blockchain, 'account3', BOOTSTRAP3_IDENTITY, BOOTSTRAP3_SECRET)
+
+
+@pytest.fixture
+def account4(blockchain):
+    return create_account(blockchain, 'account4', BOOTSTRAP4_IDENTITY, BOOTSTRAP4_SECRET)
+
+
+@pytest.fixture
+def account5(blockchain):
+    return create_account(blockchain, 'account5', BOOTSTRAP5_IDENTITY, BOOTSTRAP5_SECRET)
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def multisig(wait_transaction, blockchain, alice):
+    multisig_contract = MultisigContract.objects.create(
+        admin=alice,
+        sender=alice,
+    )
+
+    wait_transaction(multisig_contract.origination)
+
+    assert blockchain.provider.spool() is None
+
+    return multisig_contract
