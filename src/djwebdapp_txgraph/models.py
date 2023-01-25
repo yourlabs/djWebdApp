1417f49921e8eb1cd850a814245d4bb33330cc4d
Nathan Pyratz <nathanpyratz@macbook-air-de-nathan.home>
2023-01-25 17:00:37 +0100

    Implement add_edge

--- src/djwebdapp_txgraph/models.py
+++ src/djwebdapp_txgraph/models.py
@@ -28,6 +28,7 @@ class TransactionGraph(models.Model):
         G = nx.DiGraph()
         for edge in edges_qs.select_related("input_node").all():
             if edge.input_node.state != "done":
+                breakpoint()
                 G.add_edge(edge.input_node_id, edge.output_node_id)
             else:
                 G.add_node(edge.output_node_id)
@@ -38,3 +39,31 @@ class TransactionGraph(models.Model):
             return Transaction.objects.filter(id=tx_id).first()
         else:
             return None
+
+
+    def add_edge(self, transactionEdge):
+        if not isinstance(transactionEdge, TransactionEdge):
+            raise Exception("Function parameters must be instance of TransactionEdge")
+
+        if transactionEdge.output_node.state == 'done':
+            raise Exception("Output node is already deployed.")
+
+        G = nx.DiGraph()
+
+        edges_qs = self.edges.exclude(output_node__state="done")
+        for edge in edges_qs.select_related("input_node").all():
+            if edge.input_node.state != "done":
+                G.add_edge(edge.input_node_id, edge.output_node_id)
+            else:
+                G.add_node(edge.output_node_id)
+        
+        G.add_edge(transactionEdge.input_node_id, transactionEdge.output_node_id)
+
+        G_undirected = G.to_undirected()
+        if not nx.is_connected(G_undirected):
+            raise Exception("Adding this edge is going to make the graph disconnected.")
+
+        if not nx.is_directed_acyclic_graph(G):
+            raise Exception("Adding this edge is going to make the graph cyclic.")
+
+        self.edges.add(transactionEdge)
--- tests/test_txgraph.py
+++ tests/test_txgraph.py
@@ -70,3 +70,136 @@ def test_get_next_deploy():
 
     node_to_deploy = graph.get_next_deploy()
     assert node_to_deploy == None
+
+
+@pytest.mark.django_db
+def test_add_edge():
+    blockchain = Blockchain.objects.create(
+        provider_class='djwebdapp_tezos.provider.TezosProvider',
+    )
+
+    tx_1 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_2 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_3 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    
+    edge_1 = TransactionEdge.objects.create(
+        input_node=tx_1,
+        output_node=tx_2,
+    )
+
+    edge_2 = TransactionEdge.objects.create(
+        input_node=tx_1,
+        output_node=tx_3,
+    )
+
+    graph = TransactionGraph.objects.create()
+    graph.add_edge(edge_1)
+    graph.add_edge(edge_2)
+
+    assert graph.edges.count() == 2
+    assert graph.edges.first() == edge_1
+    assert graph.edges.last() == edge_2
+
+
+@pytest.mark.django_db
+def test_add_edge_fails_if_param_is_not_TransactionEdge_instance():
+    graph = TransactionGraph.objects.create()
+
+    with pytest.raises(Exception) as exception:
+        graph.add_edge("not a TransactionEdge instance")
+
+    assert str(exception.value) == "Function parameters must be instance of TransactionEdge"
+
+
+@pytest.mark.django_db
+def test_add_edge_fails_if_output_node_is_already_deployed():
+    blockchain = Blockchain.objects.create(
+        provider_class='djwebdapp_tezos.provider.TezosProvider',
+    )
+
+    tx_1 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_2 = Transaction.objects.create(blockchain=blockchain, state="done")
+
+    edge = TransactionEdge.objects.create(
+        input_node=tx_1,
+        output_node=tx_2,
+    )
+
+    graph = TransactionGraph.objects.create()
+
+    with pytest.raises(Exception) as exception:
+        graph.add_edge(edge)
+
+    assert str(exception.value) == "Output node is already deployed."
+
+
+@pytest.mark.django_db
+def test_add_edge_fails_if_graph_disconnected():
+    blockchain = Blockchain.objects.create(
+        provider_class='djwebdapp_tezos.provider.TezosProvider',
+    )
+
+    tx_1 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_2 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_3 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_4 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+
+    edge_1 = TransactionEdge.objects.create(
+        input_node=tx_1,
+        output_node=tx_2,
+    )
+
+    edge_2 = TransactionEdge.objects.create(
+        input_node=tx_3,
+        output_node=tx_4,
+    )
+
+    graph = TransactionGraph.objects.create()
+    graph.add_edge(edge_1)
+
+    with pytest.raises(Exception) as exception:
+        graph.add_edge(edge_2)
+
+    assert str(exception.value) == "Adding this edge is going to make the graph disconnected."
+
+
+@pytest.mark.django_db
+def test_add_edge_fails_if_graph_has_cycle():
+    blockchain = Blockchain.objects.create(
+        provider_class='djwebdapp_tezos.provider.TezosProvider',
+    )
+
+    tx_1 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_2 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_3 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+    tx_4 = Transaction.objects.create(blockchain=blockchain, state="deploy")
+
+    edge_1 = TransactionEdge.objects.create(
+        input_node=tx_1,
+        output_node=tx_2,
+    )
+
+    edge_2 = TransactionEdge.objects.create(
+        input_node=tx_2,
+        output_node=tx_3,
+    )
+
+    edge_3 = TransactionEdge.objects.create(
+        input_node=tx_3,
+        output_node=tx_4,
+    )
+
+    edge_4 = TransactionEdge.objects.create(
+        input_node=tx_4,
+        output_node=tx_1,
+    )
+
+    graph = TransactionGraph.objects.create()
+    graph.add_edge(edge_1)
+    graph.add_edge(edge_2)
+    graph.add_edge(edge_3)
+
+    with pytest.raises(Exception) as exception:
+        graph.add_edge(edge_4)
+
+    assert str(exception.value) == "Adding this edge is going to make the graph cyclic."
