8aa74742fec1905cc40850cd911e832e6de59dd0
Thomas Binetruy <tbin@pyratzlabs.com>
2023-01-31 18:59:06 +0100

    refactored retrieving account for given address
    This well be useful as we'll need it later on when attributing a
    a sender to an internal call origination.

--- src/djwebdapp_tezos/provider.py
+++ src/djwebdapp_tezos/provider.py
@@ -131,6 +131,20 @@ class TezosProvider(Provider):
             contract.metadata = content
             contract.number = number
             contract.state_set('done')
+    def get_account(self, address):
+        sender = Account.objects.filter(
+            address=address,
+            blockchain=self.blockchain,
+        ).first()
+
+        if not sender:
+            sender = Account.objects.create(
+                address=address,
+                blockchain=self.blockchain,
+                index=False,
+            )
+
+        return sender
 
     def index_transaction(self, level, hash, content, caller=None,
                           number=None):
@@ -192,17 +206,7 @@ class TezosProvider(Provider):
 
         # update call
         call.metadata = content
-        call.sender = Account.objects.filter(
-            address=content['source'],
-            blockchain=self.blockchain,
-        ).first()
-
-        if not call.sender:
-            call.sender = Account.objects.create(
-                address=content['source'],
-                blockchain=self.blockchain,
-                index=False,
-            )
+        call.sender = self.get_account(content['source'])
 
         # patch against empty args in pytezes
         if 'parameters' in content:
