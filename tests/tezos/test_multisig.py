2fe068c8b758f99da21649d7d059b67dafb75680
jpic <jpic@yourlabs.org>
2023-02-03 13:02:21 +0100

    TMP try with some sleeps

--- tests/tezos/test_multisig.py
+++ tests/tezos/test_multisig.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 from djwebdapp_multisig.models import AddAuthorizedContractCall, MultisigContract
 
@@ -12,7 +13,9 @@ def test_deploy_multisig(deploy_and_index, account1):
     )
     assert multisig_contract.micheline
 
+    time.sleep(5)
     deploy_and_index(multisig_contract)
+    time.sleep(5)
 
     client = multisig_contract.sender.provider.client
     multisig_interface = client.contract(multisig_contract.address)
@@ -27,7 +30,9 @@ def test_add_authorized_contract(deploy_and_index, account1):
         state='deploy',
     )
 
+    time.sleep(5)
     deploy_and_index(multisig_contract)
+    time.sleep(5)
 
     add_authorized_contract_call = AddAuthorizedContractCall.objects.create(
         sender=account1,
@@ -36,7 +41,9 @@ def test_add_authorized_contract(deploy_and_index, account1):
         state='deploy',
     )
 
+    time.sleep(5)
     deploy_and_index(add_authorized_contract_call)
+    time.sleep(5)
 
     client = multisig_contract.sender.provider.client
     multisig_interface = client.contract(multisig_contract.address)
